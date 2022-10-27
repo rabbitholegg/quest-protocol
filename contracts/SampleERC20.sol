1b62801aa8f90b57d17118f4e1f6fdac2a04c4a3
proy24 <roypritam1234@gmail.com>
2022-10-27 20:54:09 +0700

    add reward deploy tests

---
+++ contracts/SampleERC20.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.15;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @dev {ERC20} token, including:
+ *
+ *  - Preminted initial supply
+ *  - Ability for holders to burn (destroy) their tokens
+ *  - No access control mechanism (for minting/pausing) and hence no governance
+ *
+ * This contract uses {ERC20Burnable} to include burn capabilities - head to
+ * its documentation for details.
+ *
+ * _Available since v3.4._
+ *
+ * _Deprecated in favor of https://wizard.openzeppelin.com/[Contracts Wizard]._
+ */
+contract SampleERC20 is ERC20 {
+    /**
+     * @dev Mints `initialSupply` amount of token and transfers them to `owner`.
+     *
+     * See {ERC20-constructor}.
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address owner
+    ) ERC20(name, symbol) {
+        _mint(owner, initialSupply);
+    }
+}
--- test/MerkleDistributor.spec.ts
+++ test/MerkleDistributor.spec.ts
@@ -4,6 +4,24 @@ const { ethers } = require("hardhat");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Token contract", function () {
+  
+  async function deployRewardTokenFixture() {
+    // Get the ContractFactory and Signers here.
+    const RewardToken = await ethers.getContractFactory("SampleERC20");
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    // To deploy our contract, we just have to call Token.deploy() and await
+    // its deployed() method, which happens onces its transaction has been
+    // mined.
+    const expiryDate = Math.floor(Date.now() /1000) + 10000;
+
+    const hardhatRewardToken = await RewardToken.deploy("RewardToken", "RTC", 1000, owner.address);
+
+    await hardhatRewardToken.deployed();
+
+    // Fixtures can return anything you consider useful for your tests
+    return { RewardToken, hardhatRewardToken, owner, addr1, addr2 };    
+  }
 
   async function deployTokenFixture() {
     // Get the ContractFactory and Signers here.
@@ -22,6 +40,23 @@ describe("Token contract", function () {
     // Fixtures can return anything you consider useful for your tests
     return { Token, hardhatToken, owner, addr1, addr2 };
   }
+
+  async function deployAndTransferRewardToDisperser() {
+    const { RewardToken, hardhatRewardToken } = await deployRewardTokenFixture();
+    const rewardTokenAddress = hardhatRewardToken.address;
+    const rewardTokenSymbol = (await hardhatRewardToken.functions.symbol())[0];
+
+    const DisperseToken = await ethers.getContractFactory("MerkleDistributorRH");
+    const expiryDate = Math.floor(Date.now() /1000) + 10000;
+    
+    const hardhatDisperseToken = await DisperseToken.deploy(rewardTokenAddress, expiryDate);
+    await hardhatDisperseToken.deployed();
+    const disperseTokenAddresss = await hardhatDisperseToken.address;
+    await hardhatRewardToken.functions.transfer(disperseTokenAddresss, 1000);
+    return { hardhatRewardToken, rewardTokenAddress, hardhatDisperseToken, disperseTokenAddresss };
+
+  }
+
   describe("Deployment", function () {
     it("Deployment should assign the correct contract address", async function () {
       const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
@@ -57,5 +92,23 @@ describe("Token contract", function () {
       expect(getMerkleRoot).to.equal("0xdefa96435aec82d201dbd2e5f050fb4e1fef5edac90ce1e03953f916a5e0032d");    
     });    
   });
+
+  describe("Deploy and transfer rewards", function() {
+    it("Deployment should deploy reward token", async function () {
+      const { hardhatRewardToken, owner } = await loadFixture(deployRewardTokenFixture);
+      const tokenSymbol = await hardhatRewardToken.symbol();
+      expect(tokenSymbol).to.equal("RTC");
+    });
+    it("Deployment should deploy and mint reward token", async function () {
+      const { hardhatRewardToken, owner } = await loadFixture(deployRewardTokenFixture);
+      const supply = await hardhatRewardToken.totalSupply();
+      expect(supply).to.equal(1000);
+    });   
+    it("Deployment should mint and transfer rewad token to disperse contract", async function() {
+      const {hardhatDisperseToken, disperseTokenAddresss, hardhatRewardToken, rewardTokenAddress } = await loadFixture(deployAndTransferRewardToDisperser);
+      const disperseTokenBalance = (await hardhatRewardToken.functions.balanceOf(disperseTokenAddresss)).toString();
+      expect(disperseTokenBalance).to.equal('1000');
+    }); 
+  }); 
 
 });
