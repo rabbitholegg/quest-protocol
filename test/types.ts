9aa32bc9c5c3567b5e0f1dab1523e7c5935b2a5c
Lint Action <lint-action@samuelmeuli.com>
2022-12-02 17:46:41 +0000

    Fix code style issues with Prettier

--- test/types.ts
+++ test/types.ts
@@ -1,27 +1,27 @@
 export type MerkleDistributorContractType = {
-    token: () => Promise<string>,
-    totalAmount: () => Promise<number>,
-    owner: () => Promise<string>,
-    startTime: () => Promise<number>,
-    endTime: () => Promise<number>,
-    hasStarted: () => Promise<boolean>,
-    merkleRoot: () => Promise<void>,
-    address: string,
-    connect: (address: string) => ({
-        setMerkleRoot: (merkleRoot: string) => Promise<void>
-        start: () => Promise<void>
-        pause: () => Promise<void>
-        unPause: () => Promise<void>
-        withdraw: () => Promise<void>
-    })
-    withdraw: () => Promise<void>;
+  token: () => Promise<string>
+  totalAmount: () => Promise<number>
+  owner: () => Promise<string>
+  startTime: () => Promise<number>
+  endTime: () => Promise<number>
+  hasStarted: () => Promise<boolean>
+  merkleRoot: () => Promise<void>
+  address: string
+  connect: (address: string) => {
+    setMerkleRoot: (merkleRoot: string) => Promise<void>
+    start: () => Promise<void>
+    pause: () => Promise<void>
+    unPause: () => Promise<void>
+    withdraw: () => Promise<void>
+  }
+  withdraw: () => Promise<void>
 }
 
 export type SampleErc20Type = {
-    address: string
-    deployed: () => Promise<any>;
-    functions: {
-        transfer: (transferee: string, amount: number) => Promise<any>
-        balanceOf: (tokenAddress: string) => Promise<number>
-    }
+  address: string
+  deployed: () => Promise<any>
+  functions: {
+    transfer: (transferee: string, amount: number) => Promise<any>
+    balanceOf: (tokenAddress: string) => Promise<number>
+  }
 }
