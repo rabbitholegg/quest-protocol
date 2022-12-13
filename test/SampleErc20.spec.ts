63095e78c5297903b40185e3b7dcbe9640b26991
Wayne Hoover <w@waynehoover.com>
2022-12-13 12:12:19 -0600

    oncomment

--- test/SampleErc20.spec.ts
+++ test/SampleErc20.spec.ts
@@ -1,26 +1,26 @@
-// import {expect} from 'chai'
-// import {ethers} from 'hardhat'
+import {expect} from 'chai'
+import {ethers} from 'hardhat'
 
-// describe('Sample ERC-20 contract', async () => {
-//   let deployedErc20: any
+describe('Sample ERC-20 contract', async () => {
+  let deployedErc20: any
 
-//   beforeEach(async () => {
-//     const sampleErc20 = await ethers.getContractFactory('SampleERC20')
-//     const [owner] = await ethers.getSigners()
-//     deployedErc20 = await sampleErc20.deploy('RewardToken', 'RTC', 1000, owner.address)
-//     await deployedErc20.deployed()
-//   })
+  beforeEach(async () => {
+    const sampleErc20 = await ethers.getContractFactory('SampleERC20')
+    const [owner] = await ethers.getSigners()
+    deployedErc20 = await sampleErc20.deploy('RewardToken', 'RTC', 1000, owner.address)
+    await deployedErc20.deployed()
+  })
 
-//   describe('Deployment', () => {
-//     it('deploys a mock erc20', async () => {
-//       console.log()
-//       const tokenSymbol = await deployedErc20.symbol()
-//       expect(tokenSymbol).to.equal('RTC')
-//     })
+  describe('Deployment', () => {
+    it('deploys a mock erc20', async () => {
+      console.log()
+      const tokenSymbol = await deployedErc20.symbol()
+      expect(tokenSymbol).to.equal('RTC')
+    })
 
-//     it('deploys with 1000 reward', async () => {
-//       const totalSupply = await deployedErc20.totalSupply()
-//       expect(totalSupply).to.equal(1000)
-//     })
-//   })
-// })
+    it('deploys with 1000 reward', async () => {
+      const totalSupply = await deployedErc20.totalSupply()
+      expect(totalSupply).to.equal(1000)
+    })
+  })
+})
