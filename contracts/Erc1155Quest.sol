12fc38a70fcbaeacd555196fd491cdf4a6eecd61
Wayne Hoover <w@waynehoover.com>
2022-12-28 08:36:02 -0800

    cleanup imports

--- contracts/Erc1155Quest.sol
+++ contracts/Erc1155Quest.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
-import '@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol';
+import {IERC1155} from '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import {ERC1155Holder} from '@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol';
 import {Quest} from './Quest.sol';
 
 contract Erc1155Quest is Quest, ERC1155Holder {
--- contracts/Quest.sol
+++ contracts/Quest.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import {MerkleProof} from '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 import {IQuest} from './interfaces/IQuest.sol';
 import {RabbitHoleReceipt} from './RabbitHoleReceipt.sol';
-import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
+import {ECDSA} from '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 
 contract Quest is Ownable, IQuest {
     RabbitHoleReceipt public rabbitholeReceiptContract;
