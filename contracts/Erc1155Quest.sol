0762977b1a51916511c8bc531513e141b245b759
garrettjmu <garretthughes3@gmail.com>
2023-01-21 19:32:40 -0500

    add in comments for 1155quest

--- contracts/Erc1155Quest.sol
+++ contracts/Erc1155Quest.sol
@@ -5,7 +5,11 @@ import {IERC1155} from '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import {ERC1155Holder} from '@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol';
 import {Quest} from './Quest.sol';
 
+/// @title Erc1155Quest
+/// @author RabbitHole.gg
+/// @dev This contract is used to create quests with a reward token that implements the ERC1155 standard
 contract Erc1155Quest is Quest, ERC1155Holder {
+
     constructor(
         address rewardTokenAddress_,
         uint256 endTime_,
@@ -25,20 +29,28 @@ contract Erc1155Quest is Quest, ERC1155Holder {
         receiptContractAddress_
     ){}
 
+    /// @dev Checks the balance to ensure that it has enough for all of the participants. Only able to be called by owner
     function start() public override {
         if (IERC1155(rewardToken).balanceOf(address(this), rewardAmountInWeiOrTokenId) < totalParticipants)
             revert TotalAmountExceedsBalance();
         super.start();
     }
 
+    /// @dev Checks the balance to ensure that it has enough for all of the participants. Only able to be called by owner
+    /// @param amount_ The amount of reward tokens to transfer
     function _transferRewards(uint256 amount_) internal override {
         IERC1155(rewardToken).safeTransferFrom(address(this), msg.sender, rewardAmountInWeiOrTokenId, amount_, '0x00');
     }
 
+    /// @dev Returns the amount of rewards. Since an 1155 is just one token, this returns itself
+    /// @param redeemableTokenCount_ The amount of reward tokens that the user is eligible for
+    /// @return The amount of reward tokens that the user is eligible for
     function _calculateRewards(uint256 redeemableTokenCount_) internal pure override returns (uint256) {
         return redeemableTokenCount_;
     }
 
+    /// @dev Withdraws the remaining tokens from the contract. Only able to be called by owner
+    /// @param to_ The address to send the remaining tokens to
     function withdrawRemainingTokens(address to_) public override onlyOwner {
         super.withdrawRemainingTokens(to_);
         IERC1155(rewardToken).safeTransferFrom(
