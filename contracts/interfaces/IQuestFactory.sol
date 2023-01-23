9052adef3fb97560406aeb17d41ac92c0048eb9a
Garrett Hughes <garretthughes3@gmail.com>
2023-01-23 14:48:41 -0500

    Update contracts/interfaces/IQuestFactory.sol

--- contracts/interfaces/IQuestFactory.sol
+++ contracts/interfaces/IQuestFactory.sol
@@ -14,5 +14,5 @@ interface IQuestFactory {
     error QuestFeeTooHigh();
 
     event QuestCreated(address indexed creator, address indexed contractAddress, string indexed questId, string contractType, address rewardTokenAddress, uint256 endTime, uint256 startTime, uint256 totalParticipants, uint256 rewardAmountOrTokenId);
-    event ReceiptMinted(address indexed recipient, string indexed questId_);
+    event ReceiptMinted(address indexed recipient, string indexed questId);
 }
