4550670a529d763fef518720653158c31d9d4654
Jonathan Diep <jonathandiep@users.noreply.github.com>
2023-01-30 19:33:46 -0800

    Update addresses in scripts

--- scripts/upgradeRabbitHoleReceipt.js
+++ scripts/upgradeRabbitHoleReceipt.js
@@ -3,20 +3,20 @@ require('@nomiclabs/hardhat-ethers')
 const { ethers } = require('hardhat')
 
 async function main() {
-  hre.run("compile");
-  const address = "0x97a1F6Eb42DDD89ddf7E2745472D8b393970e011"; // goerli
+  hre.run('compile')
+  const address = '0x61A8CC96a3576C2a50716a0cDE70BF373C018aa6' // goerli
   const contract = await ethers.getContractFactory('RabbitHoleReceipt')
 
-  const implAddress = await upgrades.erc1967.getImplementationAddress(address);
-  console.log("Old implementation address:", implAddress);
+  const implAddress = await upgrades.erc1967.getImplementationAddress(address)
+  console.log('Old implementation address:', implAddress)
 
-  const proposal = await hre.defender.proposeUpgrade(address, contract);
-  console.log("Upgrade proposal created at:", proposal.url);
+  const proposal = await hre.defender.proposeUpgrade(address, contract)
+  console.log('Upgrade proposal created at:', proposal.url)
 
-  const newImplAddress = proposal.metadata.newImplementationAddress;
-  console.log("verifying new implementation: ", newImplAddress);
-  await hre.run("verify:verify", {address: newImplAddress});
-};
+  const newImplAddress = proposal.metadata.newImplementationAddress
+  console.log('verifying new implementation: ', newImplAddress)
+  await hre.run('verify:verify', { address: newImplAddress })
+}
 
 main()
   // eslint-disable-next-line no-process-exit
--- scripts/upgradeRabbitHoleTickets.js
+++ scripts/upgradeRabbitHoleTickets.js
@@ -3,20 +3,20 @@ require('@nomiclabs/hardhat-ethers')
 const { ethers } = require('hardhat')
 
 async function main() {
-  hre.run("compile");
-  const address = "0xC5340D9c3bDE433492D1Af95AD353B74C7DbD98C"; // goerli
+  hre.run('compile')
+  const address = '0x5C3eBe0C4a1F1505a4A106859CaBdca0913fa42F' // goerli
   const contract = await ethers.getContractFactory('RabbitHoleTickets')
 
-  const implAddress = await upgrades.erc1967.getImplementationAddress(address);
-  console.log("Old implementation address:", implAddress);
+  const implAddress = await upgrades.erc1967.getImplementationAddress(address)
+  console.log('Old implementation address:', implAddress)
 
-  const proposal = await hre.defender.proposeUpgrade(address, contract);
-  console.log("Upgrade proposal created at:", proposal.url);
+  const proposal = await hre.defender.proposeUpgrade(address, contract)
+  console.log('Upgrade proposal created at:', proposal.url)
 
-  const newImplAddress = proposal.metadata.newImplementationAddress;
-  console.log("verifying new implementation: ", newImplAddress);
-  await hre.run("verify:verify", {address: newImplAddress});
-};
+  const newImplAddress = proposal.metadata.newImplementationAddress
+  console.log('verifying new implementation: ', newImplAddress)
+  await hre.run('verify:verify', { address: newImplAddress })
+}
 
 main()
   // eslint-disable-next-line no-process-exit
