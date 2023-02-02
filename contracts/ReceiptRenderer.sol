5a48db3e811f4ec084dcc6e2b0acdf12e81811cb
Wayne Hoover <w@waynehoover.com>
2023-02-02 15:16:40 -0600

    update tests

--- contracts/ReceiptRenderer.sol
+++ contracts/ReceiptRenderer.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.15;
 import '@openzeppelin/contracts/utils/Base64.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+// import console log
+import 'hardhat/console.sol';
 
 /// @title ReceiptRenderer
 /// @author RabbitHole.gg
@@ -132,16 +134,115 @@ contract ReceiptRenderer {
         return string(text);
     }
 
-    /// @dev Returns the human readable reward amount and symbol if the reward is an ERC20 token
+    /// @dev Returns a human readable reward amount and symbol
     /// @param rewardAmount_ The reward amount
     /// @param rewardAddress_ The reward address
     function humanRewardAmountAndSymbol(uint rewardAmount_, address rewardAddress_) public view returns (string memory) {
+        string memory symbol;
+        uint8 decimals;
+
         if (rewardAddress_ == address(0)) {
-            uint humanRewardAmount = rewardAmount_ / 10 ** 18;
-            return string(abi.encodePacked(humanRewardAmount , ' ETH'));
+            symbol = 'ETH';
+            decimals = 18;
         } else {
-            uint humanRewardAmount = rewardAmount_ / 10 ** ERC20(rewardAddress_).decimals();
-            return string(abi.encodePacked(humanRewardAmount, ' ', ERC20(rewardAddress_).symbol()));
+            symbol = ERC20(rewardAddress_).symbol();
+            decimals = ERC20(rewardAddress_).decimals();
         }
+
+        return string(abi.encodePacked(decimalString(rewardAmount_, decimals, false), ' ', symbol));
+    }
+
+    /// @notice From https://gist.github.com/wilsoncusack/d2e680e0f961e36393d1bf0b6faafba7
+    struct DecimalStringParams {
+        // significant figures of decimal
+        uint256 sigfigs;
+        // length of decimal string
+        uint8 bufferLength;
+        // ending index for significant figures (funtion works backwards when copying sigfigs)
+        uint8 sigfigIndex;
+        // index of decimal place (0 if no decimal)
+        uint8 decimalIndex;
+        // start index for trailing/leading 0's for very small/large numbers
+        uint8 zerosStartIndex;
+        // end index for trailing/leading 0's for very small/large numbers
+        uint8 zerosEndIndex;
+        // true if decimal number is less than one
+        bool isLessThanOne;
+        // true if string should include "%"
+        bool isPercent;
+    }
+
+    function decimalString(uint256 number, uint8 decimals, bool isPercent) internal pure returns(string memory){
+        uint8 percentBufferOffset = isPercent ? 1 : 0;
+        uint256 tenPowDecimals = 10 ** decimals;
+        uint256 temp = number;
+        uint8 digits;
+        uint8 numSigfigs;
+        while (temp != 0) {
+            if (numSigfigs > 0) {
+                // count all digits preceding least significant figure
+                numSigfigs++;
+            } else if (temp % 10 != 0) {
+                numSigfigs++;
+            }
+            digits++;
+            temp /= 10;
+        }
+
+        DecimalStringParams memory params;
+        params.isPercent = isPercent;
+        if((digits - numSigfigs) >= decimals) {
+            // no decimals, ensure we preserve all trailing zeros
+            params.sigfigs = number / tenPowDecimals;
+            params.sigfigIndex = digits - decimals;
+            params.bufferLength = params.sigfigIndex + percentBufferOffset;
+        } else {
+            // chop all trailing zeros for numbers with decimals
+            params.sigfigs = number / (10 ** (digits - numSigfigs));
+            if(tenPowDecimals > number){
+                // number is less tahn one
+                // in this case, there may be leading zeros after the decimal place
+                // that need to be added
+
+                // offset leading zeros by two to account for leading '0.'
+                params.zerosStartIndex = 2;
+                params.zerosEndIndex = decimals - digits + 2;
+                params.sigfigIndex = numSigfigs + params.zerosEndIndex;
+                params.bufferLength = params.sigfigIndex + percentBufferOffset;
+                params.isLessThanOne = true;
+            } else {
+                // In this case, there are digits before and
+                // after the decimal place
+                params.sigfigIndex = numSigfigs + 1;
+                params.decimalIndex = digits - decimals + 1;
+            }
+        }
+        params.bufferLength = params.sigfigIndex + percentBufferOffset;
+        return generateDecimalString(params);
+    }
+
+    function generateDecimalString(DecimalStringParams memory params) private pure returns (string memory) {
+        bytes memory buffer = new bytes(params.bufferLength);
+        if (params.isPercent) {
+            buffer[buffer.length - 1] = '%';
+        }
+        if (params.isLessThanOne) {
+            buffer[0] = '0';
+            buffer[1] = '.';
+        }
+
+        // add leading/trailing 0's
+        for (uint256 zerosCursor = params.zerosStartIndex; zerosCursor < params.zerosEndIndex; zerosCursor++) {
+            buffer[zerosCursor] = bytes1(uint8(48));
+        }
+        // add sigfigs
+        while (params.sigfigs > 0) {
+            if (params.decimalIndex > 0 && params.sigfigIndex == params.decimalIndex) {
+                buffer[--params.sigfigIndex] = '.';
+            }
+            buffer[--params.sigfigIndex] = bytes1(uint8(uint256(48) + (params.sigfigs % 10)));
+            params.sigfigs /= 10;
+        }
+        return string(buffer);
     }
 }
