9554012bdd439b9810aaa4bce742e172be531c8f
Wayne Hoover <w@waynehoover.com>
2023-02-07 12:31:29 -0600

    actualy nvm

--- contracts/QuestFactory.sol
+++ contracts/QuestFactory.sol
@@ -43,8 +43,6 @@ contract QuestFactory is Initializable, OwnableUpgradeable, AccessControlUpgrade
         address erc20QuestAddress_,
         address erc1155QuestAddress_
     ) public initializer {
-        if(claimSignerAddress_ == address(0) || rabbitholeReceiptContract_ == address(0) || protocolFeeRecipient_ == address(0) || erc20QuestAddress_ == address(0) || erc1155QuestAddress_ == address(0)) revert AddressZeroNotAllowed();
-
         __Ownable_init();
         __AccessControl_init();
         grantDefaultAdminAndCreateQuestRole(msg.sender);
