34439fa01b9cd37332323fddb176ef4f5ac19bc0
Wayne Hoover <w@waynehoover.com>
2023-02-09 18:57:55 -0600

    8.16

--- contracts/interfaces/IQuest.sol
+++ contracts/interfaces/IQuest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.17;
+pragma solidity =0.8.16;
 
 // TODO clean this whole thing up
 // Allows anyone to claim a token if they exist in a merkle root.
--- contracts/test/SampleErc1155.sol
+++ contracts/test/SampleErc1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.17;
+pragma solidity =0.8.16;
 
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 
