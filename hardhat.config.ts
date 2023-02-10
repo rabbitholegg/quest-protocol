2da4d5e58f80bf06823c0f26e94c621490c8086f
Jonathan Diep <jondiep1@gmail.com>
2023-02-09 17:16:08 -0800

    Update version in Hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,7 +24,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.17',
+        version: '0.8.16',
         settings: {
           optimizer: {
             enabled: true,
