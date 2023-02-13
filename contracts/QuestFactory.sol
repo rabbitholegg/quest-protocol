2d714d032454cb3ac6a3c5ce36e1da90f303d06d
Wayne <waynehoover@users.noreply.github.com>
2023-02-13 14:21:12 -0600

    add tickets setter (#99)

--- contracts/QuestFactory.sol
+++ contracts/QuestFactory.sol
@@ -190,8 +190,14 @@ contract QuestFactory is Initializable, OwnableUpgradeable, AccessControlUpgrade
 
     /// @dev set the rabbithole receipt contract
     /// @param rabbitholeReceiptContract_ The address of the rabbithole receipt contract
-    function setRabbitHoleReceiptContract(address rabbitholeReceiptContract_) public onlyOwner {
+    function setRabbitHoleReceiptContract(address rabbitholeReceiptContract_) external onlyOwner {
         rabbitHoleReceiptContract = RabbitHoleReceipt(rabbitholeReceiptContract_);
+    }
+
+    /// @dev set the rabbithole tickets contract
+    /// @param rabbitholeTicketsContract_ The address of the rabbithole tickets contract
+    function setRabbitHoleTicketsContract(address rabbitholeTicketsContract_) external onlyOwner {
+        rabbitHoleTicketsContract = RabbitHoleTickets(rabbitholeTicketsContract_);
     }
 
     /// @dev set or remave a contract address to be used as a reward
