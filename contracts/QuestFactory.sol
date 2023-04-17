4acdab6d95e9f5b0ecb92f7aa25e3225c60dd557
Wayne <W@WayneHoover.com>
2023-04-17 11:21:56 -1000

    not needed

--- contracts/QuestFactory.sol
+++ contracts/QuestFactory.sol
@@ -69,7 +69,7 @@ contract QuestFactory is Initializable, OwnableUpgradeable, AccessControlUpgrade
     modifier claimChecks(string memory questId_, bytes32 hash_, bytes memory signature_) {
         Quest storage currentQuest = quests[questId_];
         if (currentQuest.numberMinted + 1 > currentQuest.totalParticipants) revert OverMaxAllowedToMint();
-        if (currentQuest.addressMinted[msg.sender] == true) revert AddressAlreadyMinted();
+        if (currentQuest.addressMinted[msg.sender]) revert AddressAlreadyMinted();
         if (!QuestContract(currentQuest.questAddress).queued()) revert QuestNotQueued();
         if (block.timestamp < QuestContract(currentQuest.questAddress).startTime()) revert QuestNotStarted();
         if (block.timestamp > QuestContract(currentQuest.questAddress).endTime()) revert QuestEnded();
