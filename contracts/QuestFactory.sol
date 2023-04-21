a536ca6605bde08c1727a1604288118c6bbbf1b0
Wayne <W@WayneHoover.com>
2023-04-20 17:47:49 -1000

    this is better

--- contracts/QuestFactory.sol
+++ contracts/QuestFactory.sol
@@ -63,6 +63,7 @@ contract QuestFactory is Initializable, OwnableUpgradeable, AccessControlUpgrade
     /// @dev ReentrancyGuard modifier from solmate, copied here because it was added after storage layout was finalized on first deploy
     /// @dev from https://github.com/transmissions11/solmate/blob/main/src/utils/ReentrancyGuard.sol
     modifier nonReentrant() virtual {
+        if (locked == 0) locked = 1;
         require(locked == 1, "REENTRANCY");
         locked = 2;
         _;
@@ -219,11 +220,6 @@ contract QuestFactory is Initializable, OwnableUpgradeable, AccessControlUpgrade
     function setProtocolFeeRecipient(address protocolFeeRecipient_) public onlyOwner {
         if (protocolFeeRecipient_ == address(0)) revert AddressZeroNotAllowed();
         protocolFeeRecipient = protocolFeeRecipient_;
-    }
-
-    function initLocked() public onlyOwner {
-        require(locked == 0, "Already locked");
-        locked = 1;
     }
 
     /// @dev set the mintFeeRecipient
--- scripts/deployQuestContracts.js
+++ scripts/deployQuestContracts.js
@@ -3,20 +3,20 @@ require('@nomiclabs/hardhat-ethers')
 const { ethers } = require('hardhat')
 
 async function main() {
-  const Erc20Quest = await ethers.getContractFactory('Quest')
-  const erc20Quest = await Erc20Quest.deploy()
-  await erc20Quest.deployed()
-  console.log('deployed Erc20Quest to:', erc20Quest.address)
-  console.log('verifying erc20Quest: ', erc20Quest.address)
-  await hre.run('verify:verify', { address: erc20Quest.address })
+  // const Erc20Quest = await ethers.getContractFactory('Quest')
+  // const erc20Quest = await Erc20Quest.deploy()
+  // await erc20Quest.deployed()
+  // console.log('deployed Erc20Quest to:', erc20Quest.address)
+  // console.log('verifying erc20Quest: ', erc20Quest.address)
+  // await hre.run('verify:verify', { address: erc20Quest.address })
 
   // to manually upgrade the quest factory
-  // const QuestFactory = await ethers.getContractFactory('QuestFactory')
-  // const questFactory = await QuestFactory.deploy()
-  // await questFactory.deployed()
-  // console.log('deployed QuestFactory to:', questFactory.address)
-  // console.log('verifying questFactory: ', questFactory.address)
-  // await hre.run('verify:verify', { address: questFactory.address })
+  const QuestFactory = await ethers.getContractFactory('QuestFactory')
+  const questFactory = await QuestFactory.deploy()
+  await questFactory.deployed()
+  console.log('deployed QuestFactory to:', questFactory.address)
+  console.log('verifying questFactory: ', questFactory.address)
+  await hre.run('verify:verify', { address: questFactory.address })
 }
 
 main()
