b35d2d5c9106bc194e3a3eadede4182ed915c91d
Wayne <W@WayneHoover.com>
2023-05-10 16:44:48 -1000

    add QuestNFT contract with tests

---
+++ contracts/QuestNFT.sol
@@ -0,0 +1,209 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.16;
+
+import {Initializable} from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
+import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import {PausableUpgradeable} from '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
+import {OwnableUpgradeable} from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import {CountersUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import {IERC2981Upgradeable, IERC165Upgradeable} from '@openzeppelin/contracts-upgradeable/interfaces/IERC2981Upgradeable.sol';
+import {SafeERC20, IERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import {Base64} from '@openzeppelin/contracts/utils/Base64.sol';
+import {Strings} from '@openzeppelin/contracts/utils/Strings.sol';
+
+/// @title QuestNFT
+/// @author RabbitHole.gg
+/// @notice This contract is the Erc721 Quest Completion contract. It is the NFT that can be minted after a quest is completed.
+contract QuestNFT is Initializable, ERC721Upgradeable, PausableUpgradeable, OwnableUpgradeable, IERC2981Upgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    using SafeERC20 for IERC20;
+
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
+    address public protocolFeeRecipient;
+    address public minterAddress;
+    uint256 public endTime;
+    uint256 public startTime;
+    uint256 public totalParticipants;
+    uint16 public questFee;
+    string public jsonSpecCID;
+    string public imageIPFSHash;
+    string public questId;
+
+    event JsonSpecCIDSet(string cid);
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        uint256 endTime_,
+        uint256 startTime_,
+        uint256 totalParticipants_,
+        string memory questId_,
+        uint16 questFee_,
+        address protocolFeeRecipient_,
+        address minterAdress_, // should always be the QuestFactory contract
+        string memory jsonSpecCID_,
+        string memory name_,
+        string memory symbol_,
+        string memory imageIPFSHash_
+    ) external initializer {
+        require (endTime_ > block.timestamp, 'endTime_ in past');
+        require (startTime_ > block.timestamp, 'startTime_ in past');
+        require (endTime_ > startTime_, 'startTime_ before endTime_');
+        endTime = endTime_;
+        startTime = startTime_;
+        totalParticipants = totalParticipants_;
+        questId = questId_;
+        questFee = questFee_;
+        protocolFeeRecipient = protocolFeeRecipient_;
+        jsonSpecCID = jsonSpecCID_;
+        minterAddress = minterAdress_;
+        imageIPFSHash = imageIPFSHash_;
+        __ERC721_init(name_, symbol_);
+        __Ownable_init();
+        __Pausable_init();
+    }
+
+    /// @notice Prevents reward withdrawal until the Quest has ended
+    modifier onlyAfterQuestEnd() {
+        require (block.timestamp > endTime, 'Quest has not ended');
+        _;
+    }
+
+    /// @notice Checks if quest has started from the start time
+    modifier onlyQuestBetweenStartEnd() {
+        require(block.timestamp > startTime, 'Quest not started');
+        require(block.timestamp < endTime, 'Quest ended');
+        _;
+    }
+
+    modifier onlyMinter() {
+        require(msg.sender == minterAddress, 'Only minter address');
+        _;
+    }
+
+    /// @dev set jsonSpecCID only if its empty
+    /// @param jsonSpecCID_ The jsonSpecCID to set
+    function setJsonSpecCID(string memory jsonSpecCID_) external onlyOwner {
+        require(bytes(jsonSpecCID_).length > 0, 'jsonSpecCID cannot be empty');
+        require(bytes(jsonSpecCID).length == 0, 'jsonSpecCID already set');
+
+        jsonSpecCID = jsonSpecCID_;
+        emit JsonSpecCIDSet(jsonSpecCID_);
+    }
+
+    /// @dev The maximum amount of coins the quest needs for the protocol fee
+    function totalTransferAmount() external view returns (uint256) {
+        return questFee * totalParticipants;
+    }
+
+    /// @notice Pauses the Quest
+    /// @dev Only the owner of the Quest can call this function.
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @notice Unpauses the Quest
+    /// @dev Only the owner of the Quest can call this function.
+    function unPause() external onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to_) public onlyMinter onlyQuestBetweenStartEnd whenNotPaused {
+        require (address(this).balance >= this.totalTransferAmount(), 'balance not gte totalTransferAmount');
+
+        _tokenIdCounter.increment();
+        uint tokenId = _tokenIdCounter.current();
+        _safeMint(to_, tokenId);
+        payable(protocolFeeRecipient).transfer(questFee);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    /// @dev Function that allows the owner to withdraw the remaining coins in the contract
+    /// @notice This function can only be called after the quest end time
+    function withdrawRemainingTokens() external onlyOwner onlyAfterQuestEnd {
+        uint balance = address(this).balance;
+        if (balance > 0) payable(msg.sender).transfer(balance);
+
+    }
+
+    /// @dev saftey net function to transfer tokens sent to the contract to the contract owner.
+    /// @param erc20Address_ The address of the ERC20 token to refund
+    function refund(address erc20Address_) external onlyOwner {
+        uint erc20Balance = IERC20(erc20Address_).balanceOf(address(this));
+        if (erc20Balance > 0) IERC20(erc20Address_).safeTransfer(msg.sender, erc20Balance);
+    }
+
+    /// @dev returns the token uri
+    /// @param tokenId_ the token id
+    function tokenURI(uint256 tokenId_)
+        public
+        view
+        override(ERC721Upgradeable)
+        returns (string memory)
+    {
+        bytes memory dataURI = generateDataURI(tokenId_);
+        return string(abi.encodePacked('data:application/json;base64,', Base64.encode(dataURI)));
+    }
+
+    /// @dev returns the data uri in json format
+    /// @param tokenId_ the token id
+    function generateDataURI(
+        uint tokenId_
+    ) internal view virtual returns (bytes memory) {
+        bytes memory dataURI = abi.encodePacked(
+            '{',
+            '"name": "',
+            name(),
+            '",',
+            '"description": "The RabbitHole.gg Quest Completion NFT",',
+            '"image": "',
+            tokenImage(),
+            '",',
+            '"attributes": []',
+            '}'
+        );
+        return dataURI;
+    }
+
+    function tokenImage() internal view virtual returns (string memory) {
+        return string(abi.encodePacked('ipfs://', imageIPFSHash));
+    }
+
+    /// @dev See {IERC165-royaltyInfo}
+    /// @param tokenId_ the token id
+    /// @param salePrice_ the sale price
+    function royaltyInfo(
+        uint256 tokenId_,
+        uint256 salePrice_
+    ) external view override returns (address receiver, uint256 royaltyAmount) {
+        require(_exists(tokenId_), 'Nonexistent token');
+
+        uint256 royaltyPayment = (salePrice_ * 1_000) / 10_000; // 10% royalty
+        return (owner(), royaltyPayment);
+    }
+
+    /// @dev returns true if the supplied interface id is supported
+    /// @param interfaceId_ the interface id
+    function supportsInterface(
+        bytes4 interfaceId_
+    ) public view virtual override(ERC721Upgradeable, IERC165Upgradeable) returns (bool) {
+        return interfaceId_ == type(IERC2981Upgradeable).interfaceId || super.supportsInterface(interfaceId_);
+    }
+
+    // Receive function to receive ETH
+    receive() external payable {}
+
+    // Fallback function to receive ETH when other functions are not available
+    fallback() external payable {}
+}
---
+++ test/QuestNFT.spec.ts
@@ -0,0 +1,96 @@
+import { expect } from 'chai'
+import { Contract } from 'ethers'
+import { ethers, upgrades } from 'hardhat'
+import { time } from '@nomicfoundation/hardhat-network-helpers'
+
+describe('QuestNFT Contract', async () => {
+  let questNFT: Contract,
+    questFactory: Contract,
+    contractOwner: { address: String },
+    royaltyRecipient: { address: String },
+    minterAddress: { address: String },
+    firstAddress: { address: String },
+    expiryDate: number,
+    startDate: number
+
+  beforeEach(async () => {
+    ;[contractOwner, royaltyRecipient, minterAddress, firstAddress] = await ethers.getSigners()
+    const QuestNFT = await ethers.getContractFactory('QuestNFT')
+    const QuestFactory = await ethers.getContractFactory('QuestFactory')
+    const erc20QuestContract = await ethers.getContractFactory('Quest')
+
+    const deployedErc20Quest = await erc20QuestContract.deploy()
+
+    const latestTime = await time.latest()
+    expiryDate = latestTime + 10000
+    startDate = latestTime + 10
+
+    questFactory = await upgrades.deployProxy(QuestFactory, [
+      royaltyRecipient.address,
+      firstAddress.address, // really RH Receipt contract but doesnt matter here
+      royaltyRecipient.address,
+      deployedErc20Quest.address,
+      contractOwner.address,
+      firstAddress.address, // really questTerminalKey address but doesnt matter here
+    ])
+
+    questNFT = await upgrades.deployProxy(QuestNFT, [
+      expiryDate,
+      startDate,
+      5, // uint256 totalParticipants_,
+      'quest1', // string memory questId_,
+      100, // uint16 questFee_,
+      royaltyRecipient.address, // address protocolFeeRecipient_,
+      minterAddress.address, // address minterAddress_,
+      '', // string memory jsonSpecCID_, // blank on purpose
+      'NFT Name', // string memory name_,
+      'NFTN', // string memory symbol_,
+      'imageipfs', // string memory imageIPFSHash_
+    ])
+  })
+
+  describe('Deployment', () => {
+    it('deploys and initializes the the 721 correctly', async () => {
+      expect(await questNFT.symbol()).to.equal('NFTN')
+      expect(await questNFT.name()).to.equal('NFT Name')
+      expect(await questNFT.protocolFeeRecipient()).to.equal(royaltyRecipient.address)
+    })
+  })
+
+  describe('mint', () => {
+    it('mints a token with correct questId', async () => {
+      await time.setNextBlockTimestamp(startDate + 1)
+      const transferAmount = await questNFT.totalTransferAmount()
+
+      await contractOwner.sendTransaction({
+        to: questNFT.address,
+        value: transferAmount.toNumber(),
+      })
+
+      await questNFT.connect(minterAddress).safeMint(firstAddress.address)
+
+      expect(await questNFT.balanceOf(firstAddress.address)).to.eq(1)
+      expect(await questNFT.ownerOf(1)).to.eq(firstAddress.address)
+
+      const base64encoded = await questNFT.tokenURI(1)
+      const metadata = Buffer.from(base64encoded.replace('data:application/json;base64,', ''), 'base64').toString(
+        'ascii'
+      )
+
+      const expectedMetadata = {
+        name: 'NFT Name',
+        description: 'The RabbitHole.gg Quest Completion NFT',
+        image: 'ipfs://imageipfs',
+        attributes: [], // todo is it okay to remove this key in the opensea metadata?
+      }
+
+      expect(JSON.parse(metadata)).to.eql(expectedMetadata)
+    })
+
+    it('reverts if not called by minter address', async () => {
+      await expect(questNFT.connect(firstAddress).safeMint(firstAddress.address)).to.be.revertedWith(
+        'Only minter address'
+      )
+    })
+  })
+})
