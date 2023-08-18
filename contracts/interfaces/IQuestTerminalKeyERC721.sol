5c7502611283b29785fd233c39af0ca1b6579703
Quazia <alf40k@gmail.com>
2023-08-18 19:34:15 -0400

    🔥QuestTerminalKey: contract, interface, and tests

--- contracts/interfaces/IQuestTerminalKeyERC721.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
-
-import {IERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
-import {IQuestTerminalKey} from "./IQuestTerminalKey.sol";
-
-// solhint-disable-next-line no-empty-blocks
-interface IQuestTerminalKeyERC721 is IQuestTerminalKey, IERC721Upgradeable {}
