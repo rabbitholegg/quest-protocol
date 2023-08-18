2d07411f70aa4b8482be04507a885c5999bd1316
Quazia <alf40k@gmail.com>
2023-08-18 10:27:36 -0400

    🚨solhint: no-empty-blocks

--- contracts/interfaces/IQuestOwnable.sol
+++ contracts/interfaces/IQuestOwnable.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.18;
 import {IOwnable} from "./IOwnable.sol";
 import {IQuest} from "./IQuest.sol";
 
+// solhint-disable-next-line no-empty-blocks
 interface IQuestOwnable is IQuest, IOwnable {}
--- contracts/interfaces/IQuestTerminalKeyERC721.sol
+++ contracts/interfaces/IQuestTerminalKeyERC721.sol
@@ -4,4 +4,5 @@ pragma solidity ^0.8.18;
 import {IERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import {IQuestTerminalKey} from "./IQuestTerminalKey.sol";
 
+// solhint-disable-next-line no-empty-blocks
 interface IQuestTerminalKeyERC721 is IQuestTerminalKey, IERC721Upgradeable {}
