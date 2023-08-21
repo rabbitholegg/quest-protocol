2594d279b201798efaf47630d4af80c9cb393c80
Quazia <alf40k@gmail.com>
2023-08-21 19:59:32 -0400

    🎨QuestFactoryMock: format

--- test/mocks/QuestFactoryMock.sol
+++ test/mocks/QuestFactoryMock.sol
@@ -1,15 +1,14 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.19;
 
-contract QuestFactoryMock  {
-  
-  uint256 numberMinted;
+contract QuestFactoryMock {
+    uint256 numberMinted;
 
-  function setNumberMinted(uint256 numberminted_) external {
-    numberMinted = numberminted_;
-  }
-  
-  function getNumberMinted() external view returns (uint256) {
-    return numberMinted;
-  }
+    function setNumberMinted(uint256 numberminted_) external {
+        numberMinted = numberminted_;
+    }
+
+    function getNumberMinted(string memory questId_) external view returns (uint256) {
+        return numberMinted;
+    }
 }
