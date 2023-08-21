ae04a619b29c5fd72ff5bdad57fd992ff41fa917
Quazia <alf40k@gmail.com>
2023-08-21 16:52:12 -0400

    🔨TestUtils: add reward/fee helpers

---
+++ test/helpers/TestUtils.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+
+contract TestUtils {
+    function calculateTotalRewardsPlusFee(
+        uint256 totalParticipants,
+        uint256 rewardAmount,
+        uint16 questFee
+    ) internal pure returns (uint256) {
+        return calculateTotalRewards(totalParticipants, rewardAmount)
+            + calculateTotalFees(totalParticipants, rewardAmount, questFee);
+    }
+
+    function calculateTotalRewards(uint256 totalParticipants, uint256 rewardAmount) internal pure returns (uint256) {
+        return totalParticipants * rewardAmount;
+    }
+
+    function calculateTotalFees(
+        uint256 totalParticipants,
+        uint256 rewardAmount,
+        uint16 questFee
+    ) internal pure returns (uint256) {
+        return (totalParticipants * rewardAmount * questFee) / 10_000;
+    }
+}
