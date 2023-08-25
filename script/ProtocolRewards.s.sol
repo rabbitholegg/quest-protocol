61f996d51f0f690926f5006c2eedbbb9a2b6791a
Wayne <W@WayneHoover.com>
2023-08-25 06:59:18 -1000

    add caller address to salt to prevent front-running

--- script/ProtocolRewards.s.sol
+++ script/ProtocolRewards.s.sol
@@ -8,7 +8,7 @@ import {ERC1967Factory} from "solady/src/utils/ERC1967Factory.sol";
 
 // # To deploy and verify this contract run this below, replacing sepolia with the rpc alias of the network you are deploying on
 // forge script script/ProtocolRewards.s.sol:ProtocolRewardsScript --rpc-url sepolia --broadcast --verify -vvvv
-// this should deploy to 0x7dfc6ae9c532ec324ecd5ce027c425767fcda757
+// this should deploy to 0x168437d131f8def2d94b555ff34f4539458dd6f9
 
 // to upgrade this contract
 // 1. deploy a new implementation (script todo)
@@ -17,14 +17,15 @@ import {ERC1967Factory} from "solady/src/utils/ERC1967Factory.sol";
 contract ProtocolRewardsScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("MAINNET_PRIVATE_KEY");
+
         vm.startBroadcast(deployerPrivateKey);
 
+        address owner = vm.envAddress("MAINNET_PRIVATE_KEY_PUBLIC_ADDRESS");
         address protocolRewardsImp = address(new ProtocolRewards());
         ERC1967Factory factory = ERC1967Factory(ERC1967FactoryConstants.ADDRESS);
-        address owner = vm.envAddress("MAINNET_PRIVATE_KEY_PUBLIC_ADDRESS");
-
         bytes memory initializeCallData = abi.encodeWithSignature("initialize(address)", owner);
-        bytes32 salt = 0x0000000000000000000000000000000000000000000000000000000000000001;
+        // The factory will revert if the the caller is not the first 20 bytes of the salt; preventing front-running
+        bytes32 salt = bytes32(bytes20(owner));
 
         factory.deployDeterministicAndCall(protocolRewardsImp, owner, salt, initializeCallData);
 
