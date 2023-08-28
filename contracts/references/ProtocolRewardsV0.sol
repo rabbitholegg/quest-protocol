c094b8d310718aeedef2b29184639adec53ea8f6
Wayne <W@WayneHoover.com>
2023-08-28 13:25:49 -1000

    using node_modules for now

--- contracts/references/ProtocolRewardsV0.sol
+++ contracts/references/ProtocolRewardsV0.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {Ownable} from "solady/src/auth/Ownable.sol";
+import {OwnableRoles} from "solady/src/auth/OwnableRoles.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract ProtocolRewardsV0 is Initializable, Ownable {
+contract ProtocolRewardsV0 is Initializable, OwnableRoles {
     mapping(address => uint256) public balanceOf;
     uint256 public totalBalance;
 }
