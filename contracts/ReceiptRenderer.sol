45bd028f70c3ee1edbeb751e856e0e9678f36a3b
Wayne <W@WayneHoover.com>
2023-09-14 15:51:28 -1000

    fix to 0.8.19

--- contracts/ReceiptRenderer.sol
+++ contracts/ReceiptRenderer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+pragma solidity 0.8.19;
 
 import {Base64} from "solady/src/utils/Base64.sol";
 import {LibString} from "solady/src/utils/LibString.sol";
--- contracts/interfaces/IQuest.sol
+++ contracts/interfaces/IQuest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+pragma solidity 0.8.19;
 
 interface IQuest {
     event ClaimedSingle(address indexed account, address rewardAddress, uint256 amount);
