5f24d1e5a61eaa900291a95de320d073215290dc
Wayne <waynehoover@users.noreply.github.com>
2023-09-20 13:19:25 -1000

    move to solady ownable on the factory (#207)

--- script/QuestFactory.s.sol
+++ script/QuestFactory.s.sol
@@ -10,7 +10,7 @@ import {ProxyAdmin, ITransparentUpgradeableProxy} from "@openzeppelin/contracts/
 
 // # To Upgrade QuestFactory.sol run this command below
 // ! important: make sure storage layouts are compatible first:
-// yarn clean && forge clean && forge build && npx @openzeppelin/upgrades-core validate --contract QuestFactory
+// bun clean && forge clean && forge build && npx @openzeppelin/upgrades-core validate --contract QuestFactory
 // forge script script/QuestFactory.s.sol:QuestFactoryUpgrade --rpc-url sepolia --broadcast --verify -vvvv
 contract QuestFactoryUpgrade is Script {
     function run() external {
@@ -20,6 +20,7 @@ contract QuestFactoryUpgrade is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         ProxyAdmin(C.PROXY_ADMIN_ADDRESS).upgrade(questfactoryProxy, address(new QuestFactory()));
+        QuestFactory(C.QUEST_FACTORY_ADDRESS).setOwnerOnce(); // only needed once, delete after run
 
         vm.stopBroadcast();
     }
