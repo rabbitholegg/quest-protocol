9a1b0a304ecaf5b8a3a0ca35dfea4e3eff5f4d59
Wayne <W@WayneHoover.com>
2023-10-19 12:03:51 -1000

    remove things

--- contracts/QuestFactory.sol
+++ contracts/QuestFactory.sol
@@ -12,8 +12,6 @@ import {ECDSA} from "solady/utils/ECDSA.sol";
 import {LibClone} from "solady/utils/LibClone.sol";
 import {LibString} from "solady/utils/LibString.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
-import {JSONParserLib} from "solady/utils/JSONParserLib.sol";
-import {LibExtraString} from "contracts/libraries/LibExtraString.sol";
 // References
 import {IERC1155} from "openzeppelin-contracts/token/ERC1155/IERC1155.sol";
 import {IQuestOwnable} from "./interfaces/IQuestOwnable.sol";
@@ -32,8 +30,6 @@ contract QuestFactory is Initializable, LegacyStorage, OwnableRoles, IQuestFacto
     using LibClone for address;
     using LibString for string;
     using LibString for uint256;
-    using LibExtraString for string;
-    using JSONParserLib for *; // for JSONParserLib.Item and string
 
     /*//////////////////////////////////////////////////////////////
                                 STORAGE
