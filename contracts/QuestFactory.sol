64e17cd247458e49455206ef9080ffd64cb6e024
Wayne <W@WayneHoover.com>
2023-10-18 14:42:50 -1000

    better parseAddr

--- contracts/QuestFactory.sol
+++ contracts/QuestFactory.sol
@@ -314,7 +314,7 @@ contract QuestFactory is Initializable, LegacyStorage, OwnableRoles, IQuestFacto
         JSONParserLib.Item memory item = jsonData_.parse();
 
         string memory refString = item.at('"ref"').value().decodeString();
-        address ref_ = stringToAddress(refString);
+        address ref_ = parseAddr(refString);
         string memory questId_ = item.at('"questId"').value().decodeString();
 
         if (quests[questId_].questType.eq("erc1155")) {
@@ -325,20 +325,31 @@ contract QuestFactory is Initializable, LegacyStorage, OwnableRoles, IQuestFacto
     }
 
 // todo put in a library contract
-function stringToAddress(string memory _a) public pure returns (address) {
+function parseAddr(string memory _a) internal pure returns (address _parsedAddress) {
     bytes memory tmp = bytes(_a);
+    require(tmp.length == 42, "Invalid input length"); // Ethereum addresses have 42 characters (40 for the address, 2 for the '0x' prefix)
     uint160 iaddr = 0;
     uint160 b1;
     uint160 b2;
-    for (uint i = 2; i < 2+2*20; i += 2) {
+    for (uint i = 2; i < 2 + 2 * 20; i += 2) {
         iaddr *= 256;
         b1 = uint160(uint8(tmp[i]));
-        b2 = uint160(uint8(tmp[i+1]));
-        if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
-        else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
-        if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
-        else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
-        iaddr += (b1*16+b2);
+        b2 = uint160(uint8(tmp[i + 1]));
+        if ((b1 >= 97) && (b1 <= 102)) {
+            b1 -= 87;
+        } else if ((b1 >= 65) && (b1 <= 70)) {
+            b1 -= 55;
+        } else if ((b1 >= 48) && (b1 <= 57)) {
+            b1 -= 48;
+        }
+        if ((b2 >= 97) && (b2 <= 102)) {
+            b2 -= 87;
+        } else if ((b2 >= 65) && (b2 <= 70)) {
+            b2 -= 55;
+        } else if ((b2 >= 48) && (b2 <= 57)) {
+            b2 -= 48;
+        }
+        iaddr += (b1 * 16 + b2);
     }
     return address(iaddr);
 }
