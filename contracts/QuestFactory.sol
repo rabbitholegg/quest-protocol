8325356a6ed28436aaf1706f7bf41c617cd995ed
Wayne <W@WayneHoover.com>
2023-10-24 15:54:29 -1000

    remove console

--- contracts/QuestFactory.sol
+++ contracts/QuestFactory.sol
@@ -16,7 +16,6 @@ import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 import {IERC1155} from "openzeppelin-contracts/token/ERC1155/IERC1155.sol";
 import {IQuestOwnable} from "./interfaces/IQuestOwnable.sol";
 import {IQuest1155Ownable} from "./interfaces/IQuest1155Ownable.sol";
-import "forge-std/console.sol";
 
 /// @title QuestFactory
 /// @author RabbitHole.gg
