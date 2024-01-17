201795cc37359e50c98b8ea6885e9ddd6e234f1d
gnomadic <edflem+github@gmail.com>
2024-01-16 21:45:53 -0500

    resuing a struct

---
+++ test/helpers/QuestData.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+
+contract QuestData is Test {
+
+    struct MockQuestData {
+        uint256 END_TIME;
+        uint256 START_TIME ;
+        uint256 TOTAL_PARTICIPANTS ;
+        string QUEST_ID_STRING;
+        bytes16 QUEST_ID;
+        string  ACTION_TYPE;
+        string QUEST_NAME;
+        uint32 CHAIN_ID;
+        bytes32 TX_HASH;
+        string  JSON_MSG;
+        uint256 REWARD_AMOUNT;
+    }
+  
+}
