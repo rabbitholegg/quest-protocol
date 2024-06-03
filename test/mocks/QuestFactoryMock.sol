3b00edf5e50e0a3a483b5616048289985710bdd0
Quazia <alf40k@gmail.com>
2024-06-03 10:51:02 -0400

    Test(quest factory mock): add create erc20 mocks

--- test/mocks/QuestFactoryMock.sol
+++ test/mocks/QuestFactoryMock.sol
@@ -5,6 +5,23 @@ contract QuestFactoryMock {
     uint256 numberMinted;
     uint256 public mintFee;
 
+    struct QuestData {
+        uint32 txHashChainId;
+        address rewardTokenAddress;
+        uint256 endTime;
+        uint256 startTime;
+        uint256 totalParticipants;
+        uint256 rewardAmount;
+        string questId;
+        string actionType;
+        string questName;
+        string questType;
+        string projectName;
+        uint256 referralRewardFee;
+    }
+
+    QuestData public questData;
+
     event MintFeePaid(
         string questId,
         address protocolFeeRecipient,
@@ -34,4 +51,38 @@ contract QuestFactoryMock {
     function getAddressMinted(string memory, address) external pure returns (bool) {
         return false;
     }
+
+
+    function createERC20Quest(
+        uint32 txHashChainId_,
+        address rewardTokenAddress_,
+        uint256 endTime_,
+        uint256 startTime_,
+        uint256 totalParticipants_,
+        uint256 rewardAmount_,
+        string memory questId_,
+        string memory actionType_,
+        string memory questName_,
+        string memory projectName_,
+        uint256 referralRewardFee_
+    ) external returns (address) {
+        questData = QuestData({
+            txHashChainId: txHashChainId_,
+            rewardTokenAddress: rewardTokenAddress_,
+            endTime: endTime_,
+            startTime: startTime_,
+            totalParticipants: totalParticipants_,
+            rewardAmount: rewardAmount_,
+            questId: questId_,
+            actionType: actionType_,
+            questName: questName_,
+            questType: "erc20",
+            projectName: projectName_,
+            referralRewardFee: referralRewardFee_
+        });
+
+        // Return this contract's address as the "created" quest contract
+        return address(this);
+    }
+
 }
