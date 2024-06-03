83b436743335d0da3a33044e8181bb51aa910ee4
Quazia <alf40k@gmail.com>
2024-06-02 22:26:53 -0400

    fix(quest budget): fix interface name and import

--- contracts/QuestBudget.sol
+++ contracts/QuestBudget.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.19;
+
+import {IQuestFactory} from "contracts/interfaces/IQuestFactory.sol";
 
 import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
@@ -157,7 +159,7 @@ contract QuestBudget is Budget, IERC1155Receiver, ReentrancyGuard {
         string memory projectName_,
         uint256 referralRewardFee_
     ) public virtual override onlyAuthorized returns (address) {
-        return ICreateQuest(QuestFactory).createERC20Quest(
+        return IQuestFactory(QuestFactory).createERC20Quest(
             txHashChainId_,
             rewardTokenAddress_,
             endTime_,
