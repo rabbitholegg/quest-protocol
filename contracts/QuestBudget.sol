5a63122b5d04a7f5db8e0e381e53326c67592348
mmackz <matthewmackenz@gmail.com>
2024-09-10 13:32:00 -0700

    feat: add IERC20 import to QuestBudget contract

--- contracts/QuestBudget.sol
+++ contracts/QuestBudget.sol
@@ -6,6 +6,7 @@ import {IQuestFactory} from "contracts/interfaces/IQuestFactory.sol";
 import {IERC1155Receiver} from "openzeppelin-contracts/token/ERC1155/IERC1155Receiver.sol";
 import {IERC1155} from "openzeppelin-contracts/token/ERC1155/IERC1155.sol";
 import {IERC165} from "openzeppelin-contracts/utils/introspection/IERC165.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 import {ReentrancyGuard} from "solady/utils/ReentrancyGuard.sol";
